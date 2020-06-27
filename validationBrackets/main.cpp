7da498ab791c07c2ad4cb3ae2310f1e21ff5704a
Vinnichenko-Ivan <fox4080@gmail.com>
2020-06-27 15:36:11 +0700

    update

--- validationBrackets/main.cpp
+++ validationBrackets/main.cpp
@@ -16,7 +16,9 @@ class ValidationBrackets
 		bool searchBraket(char symbol,auto &countBrakets);
 		bool coorect(auto &countBrakets,auto &brakets);
 	public:
-
+		ValidationBrackets(auto &brakets);
+		void addBrakets(auto &brakets);
+		bool assert(const string &text);
 };
 
 void outputTime(long double time)
@@ -27,10 +29,36 @@ void outputTime(long double time)
 	fout.close();
 }
 
+vector<pair<char,char>>getBrakets(int number, int &answer)
+{
+	vector<pair<char,char>>data;
+	string name="";
+
+	name="tests/keys/test"+to_string(number)+".txt";
+	cout<<name<<endl;
+	ifstream fin(name);
+	int counter;
+	string buff;
+	fin>>counter;
+	for(int i=0;i<counter;i++)
+	{
+		fin>>buff;
+		data.push_back(pair<char,char>(buff[0],buff[1]));
+	}
+	fin>>answer;
+	fin.close();
+	return data;
+}
+
 int main()
 {	
-	long double start= clock(); 
 
+	long double start= clock(); 
+	for(int i=1, answer;i<6;i++)
+	{
+		auto braketr=getBrakets(i,answer);
+		cout<<answer<<endl;
+	}
 	long double end = clock(); // засекаем время окончания
     long double t = (end - start) / CLOCKS_PER_SEC;
     cout<<t;
