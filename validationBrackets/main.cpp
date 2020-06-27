50f7c7485ff063a6ba893887bd7abecb667b2628
Vinnichenko-Ivan <fox4080@gmail.com>
2020-06-27 18:25:43 +0700

    update

--- validationBrackets/main.cpp
+++ validationBrackets/main.cpp
@@ -11,15 +11,58 @@ using namespace std;
 class ValidationBrackets
 {
 	private:
-		map<char,int>countBrakets;
-		vector<pair<char,char>>brakets;
-		bool searchBraket(char symbol,auto &countBrakets);
-		bool coorect(auto &countBrakets,auto &brakets);
+		map<char,char>pairBraketsOpen;
+		map<char,char>pairBraketsClose;
+		string braketsText="";
+		//bool coorect(auto &countBrakets,auto &inBrakets);
 	public:
-		ValidationBrackets(auto &brakets);
-		void addBrakets(auto &brakets);
-		bool assert(const string &text);
+		ValidationBrackets(auto inBrakets);
+		//void addBrakets(auto &brakets);
+		bool assert( string &text);
 };
+
+bool ValidationBrackets::assert( string &text)
+{
+	vector<char>stack;
+	for(int i=0;i<text.size();i++)
+	{
+		if(pairBraketsOpen[text[i]]!=0)
+		{
+			stack.push_back(text[i]);
+		}
+		if(pairBraketsClose[text[i]]!=0)
+		{
+			if(stack[stack.size()-1]==pairBraketsClose[text[i]]&&stack.size()>0)
+			{
+				stack.pop_back();
+			}
+			else
+			{
+				return 0;
+			}
+		}		
+	}
+	if(stack.size()==0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+	cout<<braketsText<<endl;
+}
+
+
+
+ValidationBrackets::ValidationBrackets(auto inBrakets)
+{
+	for(int i =0;i<inBrakets.size();i++)
+	{
+		pairBraketsOpen[inBrakets[i].first]=inBrakets[i].second;
+		pairBraketsClose[inBrakets[i].second]=inBrakets[i].first;
+	}
+}
 
 void outputTime(long double time)
 {	
@@ -29,14 +72,14 @@ void outputTime(long double time)
 	fout.close();
 }
 
-vector<pair<char,char>>getBrakets(int number, int &answer)
+vector<pair<char,char>>getTest(const int number, int &answer,string &text)
 {
 	vector<pair<char,char>>data;
-	string name="";
-
-	name="tests/keys/test"+to_string(number)+".txt";
-	cout<<name<<endl;
-	ifstream fin(name);
+	string nameTest="";
+	nameTest="tests/test"+to_string(number)+".txt";
+	string nameKey="";
+	nameKey="tests/keys/test"+to_string(number)+".txt";
+	ifstream fin(nameKey);
 	int counter;
 	string buff;
 	fin>>counter;
@@ -47,17 +90,22 @@ vector<pair<char,char>>getBrakets(int number, int &answer)
 	}
 	fin>>answer;
 	fin.close();
+	fin.open(nameTest);
+	fin>>text;
+	fin.close();
 	return data;
 }
 
 int main()
 {	
-
 	long double start= clock(); 
 	for(int i=1, answer;i<6;i++)
 	{
-		auto braketr=getBrakets(i,answer);
-		cout<<answer<<endl;
+		string text;
+		ValidationBrackets validationBrackets(getTest(i,answer,text));
+		cout<<endl<<"/------------------------------------------/"<<endl;
+		cout<<answer<<validationBrackets.assert(text)<<endl;
+
 	}
 	long double end = clock(); // засекаем время окончания
     long double t = (end - start) / CLOCKS_PER_SEC;
